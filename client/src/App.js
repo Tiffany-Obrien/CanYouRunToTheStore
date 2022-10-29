736d28f84f143921183d4533d4c0ca8c0dd44ce7
Erin V <erinkvoelker@gmail.com>
2022-10-29 14:05:08 -0400

    adding initial files for a PWA setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   ApolloClient,
   InMemoryCache,
@@ -18,49 +18,6 @@ import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-//list
-function App() {
-  const [list, setlist] = useState([]);
-  const [inputData, setInputData] = useState('');
-  const handleAddItem = () => {
-    const newList = [...list, {title: inputData}]
-    setlist(newList);
-    setInputData('');
-    console.log(list)
-    //needed?
-  }
-
-const handleDeleteItem = (index) => {
-  const newList = [];
-  for(let i = 0; i < list.length; i++){
-    if (index |=i) {
-      newList.push(list[i]);
-    }
-  }
-  setlist(newList);
-}
-
-return (
-  <div className="App">
-    <h1> Can you run to the store?</h1>
-    <div className='input'>
-      <input type='text' value={inputData} onChange={(event) => setInputData(event.target.value)}></input>
-      <input type='button' value='ADD' onClick={()=> handleAddItem}></input>
-    
-    </div>
-    <div className='list'>
-      {list.map((item, index) => {
-        return(
-          <div>
-           <p onClick={() => handleDeleteItem(index)}>{item.title}</p>
-            </div>
-        )
-      })}
-
-    </div>
-  </div>
-
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
